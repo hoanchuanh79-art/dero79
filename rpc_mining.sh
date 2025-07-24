126fdc7b6101c8ae0c64f6ecad150b59befd93ad
hoanchuanh79-art <hoanchuanh79@gmail.com>
2025-07-24 22:10:39 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -2,6 +2,6 @@
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
     ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm8wuv4wewq0qssenxrapS -r 
-community-pools.mysrv.cloud:10300 -m 8 -p rpc;
+community-pools.mysrv.cloud:10300 -m 7 -p rpc;
     sleep 5;
 done
